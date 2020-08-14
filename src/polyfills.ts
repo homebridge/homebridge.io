95ddcc1a0b0501ce3215b8c95577d16e84ca6716
oznu <dev@oz.nu>
2020-08-14 13:32:56 +1000

    adjust build targets to support older browsers

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -65,3 +65,4 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+import 'core-js/es/object';
